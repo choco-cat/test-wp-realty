ea935dfacf176637b229c9b7378a2066af7f627b
Panfilenka Natallia <choco-cat@mail.ru>
2024-02-03 14:23:08 +0300

    add modal template

---
+++ template-parts/modal.php
@@ -0,0 +1,17 @@
+<!-- Modal -->
+<div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><?php echo __('New Realty', THEME_TEXTDOMAIN); ?></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="<?php echo __('Close'); ?>"></button>
+            </div>
+            <div class="modal-body">
+	            <?php echo __('Realty Object Added', THEME_TEXTDOMAIN); ?>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?php echo __('Close'); ?></button>
+            </div>
+        </div>
+    </div>
+</div>
