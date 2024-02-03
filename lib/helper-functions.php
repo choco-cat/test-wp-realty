ac0e381ad9b7e8394277e009ead306a87f08ec52
Panfilenka Natallia <choco-cat@mail.ru>
2024-02-03 14:22:49 +0300

    get realty function

--- lib/helper-functions.php
+++ lib/helper-functions.php
@@ -48,3 +48,32 @@ function get_realty_types_select() {
 
 	return create_select_html( $field_name, $label, $options );
 }
+
+function get_realty_object($post_id) {
+	$thumbnail_id = get_post_thumbnail_id($post_id);
+	$thumbnail = null;
+
+	if ( $thumbnail_id ) {
+		$image_url = wp_get_attachment_image_src( $thumbnail_id, 'medium' );
+		$thumbnail = $image_url[0];
+	}
+
+	$city_id = get_post_meta( $post_id, '_selected_city', true );
+	$selected_city_title = $city_id ? get_the_title( $city_id ) : null;
+
+	return array(
+		'selected_city_title' => $selected_city_title,
+		'title'               => get_the_title($post_id),
+		'address'             => get_field( 'address', $post_id ),
+		'price'               => get_field( 'price', $post_id ),
+		'square'              => get_field( 'square', $post_id ),
+		'link'                => get_permalink($post_id),
+		'content'             => get_the_content($post_id),
+		'thumbnail'           => $thumbnail,
+	);
+}
+
+function get_template_part_with_params($path, array $params = array()) {
+	extract( $params );
+	include( locate_template( $path . '.php' ) );
+}
